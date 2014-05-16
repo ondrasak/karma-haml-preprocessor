c6f78ee6c43833b6ffaf0fb3c42b9c2edd64de40
Giovanni Carnel <giovanni@formapparatus.com>
2014-05-16 14:22:06 +0200

    added haml-coffee support

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
-var haml = require('haml');
-
 var createHamlPreprocessor = function(args, config, logger, helper) {
   config = config || {};
   var log = logger.create('preprocessor.haml');
 
+  var defaultOptions = {
+    language: 'js'
+  };
+  var options = helper.merge(defaultOptions, args.options || {}, config.options || {});
+
+  //
+
+  var transpileJS = function(content, file, done) {
+    var haml = require('haml');
+
+    var compiled = haml.compile(content);
+    done(eval(compiled));
+  };
+
+  var transpileCoffee = function(content, file, done) {
+    var hamlc = require('haml-coffee');
+
+    var compiled = hamlc.compile(content)();
+    done(compiled);
+  };
+
+  //
+
   return function(content, file, done) {
     log.debug('Processing "%s".', file.originalPath);
-    var compiled = haml.compile(content);
-    done(eval(compiled))
+
+    switch(options.language) {
+      case 'js' :
+        transpileJS(content, file, done);
+        break;
+      case 'coffee' :
+        transpileCoffee(content, file, done);
+        break;
+      default :
+        log.error('Language "%s" is not a valid source language for the hamlPreprocessor. Available options are `js` and `coffee`.', options.language);
+    }
+
   };
 };
 
-createHamlPreprocessor.$inject = ['args', 'config', 'logger', 'helper'];
+createHamlPreprocessor.$inject = ['args', 'config.hamlPreprocessor', 'logger', 'helper'];
 
 // PUBLISH DI MODULE
 module.exports = {
